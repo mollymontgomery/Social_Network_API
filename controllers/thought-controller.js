26327258d07477803d4983cb56862012fa143e52
Molly Montgomery <Molly@mollys-air-2.lan>
2022-02-13 14:50:34 -0600

    thought-controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,30 @@
+const { User, Thought } = require('../models');
+const User = require('../models/User');
+
+const thoughtController = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            // .populate('reactionId')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get one user by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
